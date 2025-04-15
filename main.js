c6b33535df82fd4a6720a045cd2d2ba06d48a32d
YarZakV2 <yarik116117.com@gmail.com>
2025-04-15 17:35:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,55 @@
-// Завдання 1
-let result = 5 + 5 + '5';
-console.log(result); 
-console.log(typeof result); 
+// 1. 
+let field1 = "Привіт";
+let field2 = "Світ";
 
-// Завдання 2
-let email = "your.email@example.com";
-console.log(email.includes('@')); 
-console.log(email.length); 
+if (field1 !== "" && field2 !== "") {
+  console.log("Обидва поля заповнені");
+} else {
+  console.log("Не всі поля заповнені");
+}
 
-// Завдання 3
-let word1 = "My";
-let word2 = "name";
-let word3 = "is";
-let fullName = `${word1} ${word2} ${word3} Viktor`;
-console.log(fullName); 
+// 2. 
+let num1 = 7;
+let num2 = 5;
+let sum = num1 + num2;
 
-// Завдання 4
-let userName = "Олександро";
-let payment = 300;
-alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
+if (sum > 10) {
+  console.log("Сума більша за 10");
+} else {
+  console.log("Сума менша або дорівнює 10");
+}
+
+// 3.
+let text = "Я вивчаю JavaScript";
+
+if (text.includes("JavaScript")) {
+  console.log("Текст містить слово JavaScript");
+} else {
+  console.log("Текст не містить слово JavaScript");
+}
+
+// 4. 
+let number = 15;
+
+if (number > 10 && number < 20) {
+  console.log("Число входить в діапазон від 10 до 20");
+} else {
+  console.log("Число не входить в діапазон від 10 до 20");
+}
+
+// 5. 
+let Name = "Ivan";
+let email = "ivan@example.com";
+let password = "123456";
+
+if (
+  Name.length >= 3 &&
+  email.includes("@") &&
+  email.includes(".") &&
+  email.indexOf("@") < email.lastIndexOf(".") &&
+  password.length >= 6
+) {
+  console.log("Перенаправлення на іншу сторінку");
+} else {
+  console.log("Помилка: неправильне заповнення");
+}
