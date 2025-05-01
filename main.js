2c46f98b7952ddafb952e13bb9c3aa5993718f80
YarZakV2 <yarik116117.com@gmail.com>
2025-05-01 18:08:21 +0300

    .

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
 // Завдання 1
-let number = 1;
-while(number < 11){
-console.log(number);
-number += 1;
-}
-// Завдання 2
-for (let i = 2; i <= 20; i++){
-  if (i % 2 !== 0) continue;
-  console.log(i);
-}
-// Завдання 3
-for (let i = 1; i <= 10; i++) {
-  console.log(`7 x ${i} = ${7 * i}`);
-}
-// Завдання 4
-let n = 15; 
+const a = [1, 2, 3];
+a[1] = 10;
+console.log(a);
 
-for (let i = 0; ; i++) {
-  if (i >= n) break;
-  console.log(i);
-}
-// Завдання 5
-let i = 1;
+//Завдання 2
+const string = ["вввв", "ііііі", "ssssss"];
+string.push("яяяяяя");
+console.log(string);
 
-while (i <= 20) {
-  if (i % 3 === 0) {
-    i++;
-    continue;
+//Завдання 3
+const num = [18, 155, 30, 48];
+let sum = 0;
+
+for (let i = 0; i < num.length; i++) {
+sum += num[i];
+}
+console.log(sum);
+
+//Завдання 4
+const number = [18, 155, 30, 48, 11];
+for (let i = 0; i < number.length; i++){
+  console.log (number[i]);
+}
+//Завдання 5
+const str = ["ssssss", "ss", "aaaaaaaaaaaa", "bbbb", "cccccccc"];
+for (let i = 0; i < str.length; i++){
+  if (str[i].length > 5) {
+    console.log(str[i]);
   }
-  console.log(i);
-  i++;
+}
+
+// Завдання 6
+const numbers = [18, 155, 30, 48, 11, 15, 100, 78, 254, 32];
+console.log(Math.max(...numbers));
+
+// Завдання 7
+const numberss = [3, 12, 7, 8, 5, 10, 2, 19, 6, 4];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    console.log(numbers[i]);
+  }
 }
