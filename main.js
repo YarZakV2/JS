4322985df894ad00e1d9fd72c2dd802eb1f7ae1d
YarZakV2 <yarik116117.com@gmail.com>
2025-05-08 17:53:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,42 @@
-// Завдання 1
-const a = [1, 2, 3];
-a[1] = 10;
-console.log(a);
-
-//Завдання 2
-const string = ["вввв", "ііііі", "ssssss"];
-string.push("яяяяяя");
+//Завдання 1
+const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+const string = friends.join(', ')
+// let string = '';
+// for (let i = 0; i < friends.length; i++){
+//   string += friends[i];
+//   if (i < friends.length - 1){
+//     string += ',';
+//   }
+// }
 console.log(string);
 
-//Завдання 3
-const num = [18, 155, 30, 48];
-let sum = 0;
+// Завдання 2
+const cards = [
+  'Картока-1',
+  'Картока-2',
+  'Картока-3',
+  'Картока-4',
+  'Картока-5',
+];
 
-for (let i = 0; i < num.length; i++) {
-sum += num[i];
-}
-console.log(sum);
+const cardToRemove = 'Картока-3';
+const cardToInsert = 'Карточка 6';
+const cardToUpdate = 'Карточка-4';
 
-//Завдання 4
-const number = [18, 155, 30, 48, 11];
-for (let i = 0; i < number.length; i++){
-  console.log (number[i]);
-}
-//Завдання 5
-const str = ["ssssss", "ss", "aaaaaaaaaaaa", "bbbb", "cccccccc"];
-for (let i = 0; i < str.length; i++){
-  if (str[i].length > 5) {
-    console.log(str[i]);
-  }
+// Видалення 
+const removeIndex = cards.indexOf(cardToRemove);
+if (removeIndex !== -1) {
+  cards.splice(removeIndex, 1);
 }
 
-// Завдання 6
-const numbers = [18, 155, 30, 48, 11, 15, 100, 78, 254, 32];
-console.log(Math.max(...numbers));
+// Додавання 
+const insertIndex = 2;
+cards.splice(insertIndex, 0, cardToInsert);
 
-// Завдання 7
-const numberss = [3, 12, 7, 8, 5, 10, 2, 19, 6, 4];
-
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] % 2 === 0) {
-    console.log(numbers[i]);
-  }
+// Оновлення
+const updateIndex = cards.indexOf('Картока-4');
+if (updateIndex !== -1) {
+  cards[updateIndex] = cardToUpdate;
 }
+
+console.log(cards);
