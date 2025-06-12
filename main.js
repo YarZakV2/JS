98e384037d6d5de8589e17d8af3e14a8c91b1f03
YarZakV2 <yarik116117.com@gmail.com>
2025-06-12 17:00:26 +0300

    .

--- main.js
+++ main.js
@@ -1,92 +1,97 @@
-const bankAccount = {
-  ownerName: "Іван Іванов",
-  accountNumber: "123456789",
-  balance: 1000,
-
-  deposit(amount) {
-    this.balance += amount;
-    alert(`Рахунок поповнено. Залишок: ${this.balance} грн.`);
-  },
-
-  withdraw(amount) {
-    if (amount <= this.balance) {
-      this.balance -= amount;
-      alert(`Гроші знято. Залишок: ${this.balance} грн.`);
-    } else {
-      alert("Недостатньо коштів на рахунку.");
-    }
-  },
-};
-
-
-if (confirm("Бажаєте поповнити рахунок?")) {
-  const amount = Number(prompt("Введіть суму для поповнення:"));
-  bankAccount.deposit(amount);
-} else if (confirm("Бажаєте зняти гроші з рахунку?")) {
-  const amount = Number(prompt("Введіть суму для зняття:"));
-  bankAccount.withdraw(amount);
-}
-
-
-const weather = {
-  temperature: null,
-  humidity: 60,
-  windSpeed: 10,
-
-  isBelowZero() {
-    return this.temperature < 0;
-  },
-};
-
-
-weather.temperature = Number(prompt("Введіть температуру в °C:"));
-if (weather.isBelowZero()) {
-  alert("Температура нижче 0 градусів Цельсія.");
-} else {
-  alert("Температура 0 або вище градусів Цельсія.");
-}
-
-
+//1
 const user = {
-  name: "Олена",
-  email: "test@example.com",
-  password: "123456",
+  name: 'John',
+  age: 30,
+  hobby: 'reading',
+  premium: true,
+};
 
-  login() {
-    const enteredEmail = prompt("Введіть email:");
-    const enteredPassword = prompt("Введіть пароль:");
-    if (enteredEmail === this.email && enteredPassword === this.password) {
-      alert("Вхід успішний!");
-    } else {
-      alert("Невірний email або пароль.");
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+for (const key of Object.keys(user)) {
+  console.log(`${key}: ${user[key]}`);
+}
+
+//2
+function countProps(obj) {
+  return Object.keys(obj).length;
+}
+console.log(countProps({ name: 'Mango', age: 2 }));
+
+//3
+function findBestEmployee(employees) {
+  let best = '';
+  let maxTasks = 0;
+
+  for (const name in employees) {
+    if (employees[name] > maxTasks) {
+      maxTasks = employees[name];
+      best = name;
     }
-  },
-};
+  }
 
+  return best;
+}
 
-user.login();
+console.log(findBestEmployee({
+  Ann: 29,
+  David: 35,
+  Helen: 1,
+  Lorence: 99
+})); 
 
-
-const movie = {
-  title: "Inception",
-  director: "Christopher Nolan",
-  year: 2010,
-  rating: 8.8,
-
-  isHighRated() {
-    return this.rating > 8;
-  },
-};
-
-
-if (movie.isHighRated()) {
-  console.log("Фільм має високий рейтинг.");
-} else {
-  console.log("Рейтинг фільму середній або низький.");
+//4
+function countTotalSalary(employees) {
+  let total = 0;
+  for (const salary of Object.values(employees)) {
+    total += salary;
+  }
+  return total;
 }
 
 
-console.log("Назва:", movie.title);
-console.log("Режисер:", movie.director);
-console.log("Рік:", movie.year);
-console.log("Рейтинг:", movie.rating);
+console.log(countTotalSalary({
+  Mango: 100,
+  Poly: 150,
+  Alfred: 80
+}));
+
+//5
+function getAllPropValues(arr, prop) {
+  const values = [];
+  for (const obj of arr) {
+    if (prop in obj) {
+      values.push(obj[prop]);
+    }
+  }
+  return values;
+}
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+];
+
+console.log(getAllPropValues(products, 'name')); 
+
+//6
+function calculateTotalPrice(allProducts, productName) {
+  for (const product of allProducts) {
+    if (product.name === productName) {
+      return product.price * product.quantity;
+    }
+  }
+  return 0;
+}
+
+
+const items = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+];
+
+console.log(calculateTotalPrice(items, 'Radar')); 
+
+
