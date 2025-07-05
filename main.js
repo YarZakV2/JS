3b3af4ad077a9549fe5aebe8ff643d4528cb572e
YarZakV2 <yarik116117.com@gmail.com>
2025-07-05 20:20:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,206 +1,199 @@
-const users = [
-
-  {
-
-    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
-
-    name: 'Moore Hensley',
-
-    email: 'moorehensley@indexia.com',
-
-    eyeColor: 'blue',
-
-    friends: ['Sharron Pace'],
-
-    isActive: false,
-
-    balance: 2811,
-
-    skills: ['ipsum', 'lorem'],
-
-    gender: 'male',
-
-    age: 37,
-
-  },
-
-  {
-
-    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
-
-    name: 'Sharlene Bush',
-
-    email: 'sharlenebush@tubesys.com',
-
-    eyeColor: 'blue',
-
-    friends: ['Briana Decker', 'Sharron Pace'],
-
-    isActive: true,
-
-    balance: 3821,
-
-    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
-
-    gender: 'female',
-
-    age: 34,
-
-  },
-
-  {
-
-    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
-
-    name: 'Ross Vazquez',
-
-    email: 'rossvazquez@xinware.com',
-
-    eyeColor: 'green',
-
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-
-    isActive: false,
-
-    balance: 3793,
-
-    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-
-    gender: 'male',
-
-    age: 24,
-
-  },
-
-  {
-
-    id: '249b6175-5c30-44c6-b154-f120923736f5',
-
-    name: 'Elma Head',
-
-    email: 'elmahead@omatom.com',
-
-    eyeColor: 'green',
-
-    friends: ['Goldie Gentry', 'Aisha Tran'],
-
-    isActive: true,
-
-    balance: 2278,
-
-    skills: ['adipisicing', 'irure', 'velit'],
-
-    gender: 'female',
-
-    age: 21,
-
-  },
-
-  {
-
-    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
-
-    name: 'Carey Barr',
-
-    email: 'careybarr@nurali.com',
-
-    eyeColor: 'blue',
-
-    friends: ['Jordan Sampson', 'Eddie Strong'],
-
-    isActive: true,
-
-    balance: 3951,
-
-    skills: ['ex', 'culpa', 'nostrud'],
-
-    gender: 'male',
-
-    age: 27,
-
-  },
-
-  {
-
-    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
-
-    name: 'Blackburn Dotson',
-
-    email: 'blackburndotson@furnigeer.com',
-
-    eyeColor: 'brown',
-
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-
-    isActive: false,
-
-    balance: 1498,
-
-    skills: ['non', 'amet', 'ipsum'],
-
-    gender: 'male',
-
-    age: 38,
-
-  },
-
-  {
-
-    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
-
-    name: 'Sheree Anthony',
-
-    email: 'shereeanthony@kog.com',
-
-    eyeColor: 'brown',
-
-    friends: ['Goldie Gentry', 'Briana Decker'],
-
-    isActive: true,
-
-    balance: 2764,
-
-    skills: ['lorem', 'veniam', 'culpa'],
-
-    gender: 'female',
-
-    age: 39,
-
-  },
-
-];
-
 //1
-const getUserNames = users => {
-  return users.map(user => user.name);
+  function Account({ login, email }) {
+  this.login = login;
+  this.email = email;
+}
+
+Account.prototype.getInfo = function () {
+  console.log(`Login: ${this.login}, Email: ${this.email}`);
 };
 
-console.log(getUserNames(users));
+console.log(Account.prototype.getInfo);
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo();
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); 
+
 //2
-const getUsersWithEyeColor = (users, color) => {
-  return users.filter(user => user.eyeColor === color);
-};
+class User {
+  constructor({ name, age, followers }) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
 
-console.log(getUsersWithEyeColor(users, 'blue'));
+  getInfo() {
+    console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
+  }
+}
+
+const mango = new User({
+  name: 'Mango',
+  age: 2,
+  followers: 20,
+});
+
+mango.getInfo();
+
+const poly = new User({
+  name: 'Poly',
+  age: 3,
+  followers: 17,
+});
+
+poly.getInfo();
+
 //3
-const getUsersWithGender = (users, gender) => {
-  return users
-    .filter(user => user.gender === gender)
-    .map(user => user.name);
-};
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
 
-console.log(getUsersWithGender(users, 'male'));
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    const index = this.items.indexOf(item);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage([
+  'Нанітоіди',
+  'Пролонгер',
+  'Залізні жупи',
+  'Антигравітатор',
+]);
+
+const items = storage.getItems();
+console.table(items);
+
+storage.addItem('Дроїд');
+console.table(storage.items);
+
+storage.removeItem('Пролонгер');
+console.table(storage.items);
 
 //4
-const getInactiveUsers = users => {
-  return users.filter(user => !user.isActive);
-};
-console.log(getInactiveUsers(users));
+class StringBuilder {
+  constructor(initialValue) {
+    this._value = initialValue;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  append(str) {
+    this._value += str;
+  }
+
+  prepend(str) {
+    this._value = str + this._value;
+  }
+
+  pad(str) {
+    this._value = str + this._value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value);
+
+builder.prepend('^');
+console.log(builder.value); 
+
+builder.pad('=');
+console.log(builder.value); 
 
 //5
-const getUserWithEmail = (users, email) => {
-  return users.find(user => user.email === email);
-};
+class Car {
+  static getSpecs(car) {
+    console.log(
+      `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`
+    );
+  }
 
-console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
-console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
+  constructor({ maxSpeed, price }) {
+    this.speed = 0;
+    this.maxSpeed = maxSpeed;
+    this._price = price;
+    this.isOn = false;
+    this.distance = 0;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(value) {
+    this._price = value;
+  }
+
+  turnOn() {
+    this.isOn = true;
+  }
+
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+
+  accelerate(value) {
+    const newSpeed = this.speed + value;
+    if (newSpeed <= this.maxSpeed) {
+      this.speed = newSpeed;
+    }
+  }
+
+  decelerate(value) {
+    const newSpeed = this.speed - value;
+    if (newSpeed >= 0) {
+      this.speed = newSpeed;
+    }
+  }
+
+  drive(hours) {
+    if (this.isOn) {
+      this.distance += this.speed * hours;
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+
+console.log(mustang.price); 
+mustang.price = 4000;
+console.log(mustang.price);
+
